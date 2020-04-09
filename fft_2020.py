74f66639213f65c0657751b20ab80ef6f46f97d0
giulio99 <56739190+giulio99@users.noreply.github.com>
2020-04-09 10:01:27 +0200

    Add files via upload

---
+++ fft_2020.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import pylab as pl
+from scipy.optimize import curve_fit
+
+
+url = "https://raw.githubusercontent.com/giulio99/Relazione-FFT/master/dati%20prof/sin11.txt"
+df = pd.read_csv(url, sep=" ",header=None)
+
+df.columns =['time', 'voltage']
+
+t1 = list(df.time)
+
+V1 = list(df.voltage) 
+
+print(t1)
+
+print(V1)
